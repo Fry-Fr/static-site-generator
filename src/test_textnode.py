49743d6bdbfc1c0caac045612bdac5fdaaa86b86
Fry-Fr <jwalker.ry@gmail.com>
2025-08-16 17:20:37 -0400

    add test script and unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,42 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_link_url_none(self):
+        node = TextNode("This is a link", TextType.LINK)
+        self.assertIsNone(node.url)
+    
+    def test_link_url_is_not_none(self):
+        node = TextNode("This is a link", TextType.LINK, url="http://example.com")
+        self.assertIsNotNone(node.url)
+    
+    def test_url_is_not_none(self):
+        node = TextNode("This is an image", TextType.IMAGE, url="http://example.com/image.png")
+        self.assertIsNotNone(node.url)
+
+    def test_url_raises_exception(self):
+        # Test that ValueError is raised when url is empty for IMAGE type
+        with self.assertRaises(ValueError) as cm_1:
+            TextNode("This is an image", TextType.IMAGE, url="")
+        self.assertEqual(str(cm_1.exception), "url must be provided for IMAGE text_type")
+        # Test that ValueError is raised when url is None for IMAGE type
+        with self.assertRaises(ValueError) as cm_2:
+            TextNode("This is an image", TextType.IMAGE)
+        self.assertEqual(str(cm_2.exception), "url must be provided for IMAGE text_type")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
