b34d0bccac41d84e439b1ee79260d2b8c6875bc4
Fry-Fr <jwalker.ry@gmail.com>
2025-08-18 21:15:14 -0400

    add text_to_textnode function and add unit tests

---
+++ src/test_text_to_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+from textnode import TextNode, TextType
+from text_to_textnode import text_to_textnode
+
+class TestTextToTextNode(unittest.TestCase):
+    # Test converting text to TextNode
+    def test_text_to_textnode(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnode(text)
+        result_nodes = [
+          TextNode("This is ", TextType.TEXT),
+          TextNode("text", TextType.BOLD),
+          TextNode(" with an ", TextType.TEXT),
+          TextNode("italic", TextType.ITALIC),
+          TextNode(" word and a ", TextType.TEXT),
+          TextNode("code block", TextType.CODE),
+          TextNode(" and an ", TextType.TEXT),
+          TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+          TextNode(" and a ", TextType.TEXT),
+          TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(len(nodes), len(result_nodes))
+        self.assertListEqual(nodes, result_nodes)
+
+    def test_empty_text(self):
+        text = ""
+        nodes = text_to_textnode(text)
+        self.assertEqual(len(nodes), 0)
+        self.assertListEqual(nodes, [])
---
+++ src/text_to_textnode.py
@@ -0,0 +1,12 @@
+from split_nodes_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
+
+def text_to_textnode(text: str) -> list[TextNode]:
+  original_text_node = TextNode(text, TextType.TEXT)
+  node_list = []
+  node_list = split_nodes_delimiter([original_text_node], '`', TextType.CODE)
+  node_list = split_nodes_delimiter(node_list, '**', TextType.BOLD)
+  node_list = split_nodes_delimiter(node_list, '_', TextType.ITALIC)
+  node_list = split_nodes_image(node_list)
+  node_list = split_nodes_link(node_list)
+  return node_list
