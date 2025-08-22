31b0d15ab230938173b01a3d30776951afdc862d
Fry-Fr <jwalker.ry@gmail.com>
2025-08-21 22:46:03 -0400

    update generate page

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-# from src.textnode import TextNode, TextType
-from recursive_copy import recursive_copy
+from src.recursive_copy import recursive_copy
+from src.generate_page import generate_page
 
 def main():
   recursive_copy("static", "public")
+  generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
   main()
