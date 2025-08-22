268ab39a284b06a44688796471e0f15e69686595
Fry-Fr <jwalker.ry@gmail.com>
2025-08-21 22:37:34 -0400

    add generate_page

---
+++ src/generate_page.py
@@ -0,0 +1,20 @@
+import os
+from src.extract_title import extract_title
+from src.markdown_to_html_node import markdown_to_html_node
+
+def generate_page(from_path, template_path, destination_path):
+    print(f"Generating page from {from_path} to {destination_path} using template {template_path}")
+    markdown_content = ""
+    with open(from_path, 'r') as f:
+        markdown_content = f.read()
+    template = ""
+    with open(template_path, 'r') as f:
+        template = f.read()
+    title = extract_title(markdown_content)
+    html_content = markdown_to_html_node(markdown_content).to_html()
+    final_content = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+
+    # Ensure destination directory exists
+    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    with open(destination_path, 'w') as f:
+        f.write(final_content)
