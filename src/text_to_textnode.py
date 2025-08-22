9fc6be20653801cb17fb8fc8b90901c28d7d4cc1
Fry-Fr <jwalker.ry@gmail.com>
2025-08-22 13:40:04 -0400

    update list variable

--- src/text_to_textnode.py
+++ src/text_to_textnode.py
@@ -2,9 +2,8 @@ from src.split_nodes_delimiter import split_nodes_delimiter, split_nodes_image, 
 from src.textnode import TextNode, TextType
 
 def text_to_textnode(text: str) -> list[TextNode]:
-  original_text_node = TextNode(text, TextType.TEXT)
-  node_list = []
-  node_list = split_nodes_delimiter([original_text_node], '`', TextType.CODE)
+  node_list = [TextNode(text, TextType.TEXT)]
+  node_list = split_nodes_delimiter(node_list, '`', TextType.CODE)
   node_list = split_nodes_delimiter(node_list, '**', TextType.BOLD)
   node_list = split_nodes_delimiter(node_list, '_', TextType.ITALIC)
   node_list = split_nodes_image(node_list)
